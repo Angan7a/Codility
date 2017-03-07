fc70f39524dd2c04694162f27cb5ac6418797376
Andrzej <angan@o2.pl>
2017-03-07 15:17:18 +0100

    Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum

--- Lesson_4_MaxCounters.cpp
+++ Lesson_4_MaxCounters.cpp
@@ -1,20 +1,25 @@
 vector<int> solution(int N, vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    int max = 0;
-    vector<int> B;
-    for (int i = 0; i < N; i++) {
-        B.push_back(0);
-    }
+    int max = 0, last_max = 0;
+    vector<int> B(N,0);
+
     for (int i = 0; i < (int)A.size(); i++) {
         if (A[i] == N + 1) {
-            for (int j = 0; j < N; j++) {
-                B[j] = max;
-            }
+                last_max = max;
         } else {
-            B[A[i] - 1] += 1;
+            if (B[A[i] - 1] < last_max) {
+                B[A[i] - 1] = last_max + 1;
+            } else {
+                B[A[i] - 1]++;
+            }
             if (B[A[i] - 1] > max) {
                 max = B[A[i] - 1];
             }
+        }
+    }
+    for (int j = 0; j < N; j++) {
+        if (B[j] < last_max) {
+            B[j] = last_max;
         }
     }
     return B;
