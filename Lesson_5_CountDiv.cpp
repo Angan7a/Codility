305fa0371fe9303c3dd7f1ebc09e0e5b9f36c412
Andrzej <angan@o2.pl>
2017-03-08 10:17:12 +0100

    Compute number of integers divisible by k in range [a..b]

---
+++ Lesson_5_CountDiv.cpp
@@ -0,0 +1,20 @@
+int solution(int A, int B, int K) {
+    // write your code in C++14 (g++ 6.2.0)
+    int i;
+    if (B == 0) {
+        return 1;
+    }
+    if (B < K) {
+        return 0;
+    }
+    if (A == B) {
+        if (B % K == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    for (i = A; (i % K) != 0; i++) {
+    }
+    return ((B - i) / K) + 1;
+}
